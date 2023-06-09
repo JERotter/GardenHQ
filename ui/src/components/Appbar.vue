6e58d357a37441cde8bfb9e4980cb75f1391fab8
jenell <Jenell.E.Rotter@phila.gov>
2023-06-09 17:27:42 -0400

    Updated the homeview news reel & fixed the appbar scrolling issue

--- ui/src/components/Appbar.vue
+++ ui/src/components/Appbar.vue
@@ -1,34 +1,45 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <v-card app >
-    <v-app-bar color="teal-darken-4" image="../assets/images/vegetables.jpeg " >
-      <template v-slot:image>
-        <v-img
-          gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
-
-      <v-app-bar-title
-        ><a href="/" style="text-decoration: none; color: white"
-          >Garden HQ</a
-        ></v-app-bar-title
+    <v-card app >
+      <v-app-bar
+        color="teal-darken-4"
+        image="../assets/images/vegetables.jpeg "
       >
+        <template v-slot:image>
+          <v-img
+            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
 
-      <v-spacer></v-spacer>
+        <v-app-bar-title
+          ><a href="/" style="text-decoration: none; color: white"
+            >Garden HQ</a
+          ></v-app-bar-title
+        >
 
-      <v-btn icon to="/dashboard" link>
-        <v-icon>mdi-login</v-icon>
-      </v-btn>
+        <v-spacer></v-spacer>
 
-      <!-- <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn> -->
-    </v-app-bar>
-  </v-card>
+        <v-btn icon to="/dashboard" link>
+          <v-icon>mdi-login</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </v-card>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+  .app {
+    position: relative;
+  }
+
+  .appbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+  }
+</style>
--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,127 +1,235 @@
-<!-- https://github.com/BrianMwengi/vuenewsapi-app/tree/master/src -->
-<!-- https://www.youtube.com/watch?v=lVr9ghYgrgU&list=PL046zjOZamPNQDJpjv8H5HFVI0PrrOlC4&index=2&ab_channel=WebDevTrainee -->
-
 <template>
-  <v-app id="inspire">
-    <appbar />
-    <v-main class="bg-brown-lighten-5">
-      <v-container>
-        <v-row>
-          <v-col cols="12" sm="2">
-            <v-sheet class="left-sheet" rounded="lg" min-height="268">
-              <div class="letf-div">
-                <h4>Resources</h4>
-                <p>paragrphs</p>
-              </div>
-            </v-sheet>
-          </v-col>
+  <v-app>
+    <div class="main">
+      <div class="appdiv">
+        <appbar class="appbar" />
+      </div>
+      <div class="carousel-container">
+        <v-carousel hide-delimiters="">
+          <v-carousel-item
+            v-for="(article, index) in articles"
+            :key="index"
+            @click="navTo(article.url)"
+          >
+            <div class="header">
+              <img
+                v-if="article.urlToImage"
+                :src="article.urlToImage"
+                alt="article.description"
+              />
+              <i v-else class="fas fa-image"></i>
+            </div>
+            <div class="article-details">
+              <a :href="article.url" class="article-title">{{
+                article.title
+              }}</a>
+            </div>
+          </v-carousel-item>
+        </v-carousel>
 
-          <v-col cols="12" sm="8">
-            <v-sheet class="news-sheet" min-height="70vh" rounded="lg">
-              <div id="app">
-                <News :apiKey="apiKey" />
-              </div>
-            </v-sheet>
-          </v-col>
+        <div class="about">
+          <div class="about">
+            <h1>About Garden HQ</h1>
+            <p>
+              Garden HQ is a comprehensive platform designed to empower
+              organizers and community members in managing and nurturing
+              community gardens. Our platform provides a centralized hub for all
+              aspects of community garden organization, fundraising, resource
+              management, and volunteer engagement.
+            </p>
+            <p>
+              At Garden HQ, we understand the importance of community gardens as
+              vibrant spaces that bring people together, promote sustainable
+              practices, and enhance the well-being of neighborhoods. We aim to
+              simplify the process of garden management, allowing organizers to
+              focus on creating thriving green spaces and fostering community
+              connections.
+            </p>
+            <h2>Features and Benefits</h2>
+            <ul>
+              <li>
+                Organize and Manage: Garden HQ provides a user-friendly
+                interface for administrators, managers, and volunteers to
+                collaborate and streamline garden-related tasks. From plot
+                assignments and event scheduling to inventory tracking and
+                maintenance logs, Garden HQ simplifies the organizational
+                process.
+              </li>
+              <li>
+                Funds and Resources: With Garden HQ, organizers can explore
+                funding opportunities and easily manage financial aspects of
+                their community garden projects. Our platform connects you with
+                potential donors, grants, and resources, ensuring sustainable
+                growth and development.
+              </li>
+              <li>
+                Education and Engagement: Garden HQ believes in the power of
+                knowledge sharing. Our platform offers educational resources,
+                articles, and forums to inspire and inform gardeners of all
+                levels. Connect with like-minded individuals, share insights,
+                and learn from experienced community garden enthusiasts.
+              </li>
+              <li>
+                Volunteer Network: Garden HQ makes it effortless to connect with
+                passionate individuals looking to contribute their time and
+                skills to community gardens. Volunteers can discover nearby
+                projects, sign up for shifts, and stay engaged in meaningful
+                gardening activities.
+              </li>
+            </ul>
+            <p>
+              Whether you're an administrator overseeing multiple community
+              gardens, a manager responsible for day-to-day operations, or a
+              volunteer eager to make a difference, Garden HQ provides the tools
+              and support you need to create successful and thriving community
+              garden spaces.
+            </p>
+          </div>
+        </div>
 
-          <v-col cols="12" sm="2">
-            <v-sheet class="right-sheet" rounded="lg" min-height="268">
-              <h3>About</h3>
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-
-    <footbar />
+        <div class="resources">
+          <h1>Resources</h1>
+          <p>
+            Lorem ipsum dolor & cheddar! sit amet consectetur adipisicing elit.
+            Sapiente vel mollitia totam eius illum dolor asperiores maiores
+            voluptates inventore consequuntur sed commodi blanditiis, repellat
+            vitae, saepe harum quos, dicta error! Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Sapiente vel mollitia totam eius illum
+            dolor asperiores maiores voluptates inventore consequuntur sed
+            commodi blanditiis, repellat vitae, saepe harum quos, dicta error!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            vel mollitia totam eius illum dolor asperiores maiores voluptates
+            inventore consequuntur sed commodi blanditiis, repellat vitae, saepe
+            harum quos, dicta error! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sapiente vel mollitia totam eius illum dolor
+            asperiores maiores voluptates inventore consequuntur sed commodi
+            blanditiis, repellat vitae, saepe harum quos, dicta error! Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel
+            mollitia totam eius illum dolor asperiores maiores voluptates
+            inventore consequuntur sed commodi blanditiis, repellat vitae, saepe
+            harum quos, dicta error! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sapiente vel mollitia totam eius illum dolor
+            asperiores maiores voluptates inventore consequuntur sed commodi
+            blanditiis, repellat vitae, saepe harum quos, dicta error! Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Sapiente vel
+            mollitia totam eius illum dolor asperiores maiores voluptates
+            inventore consequuntur sed commodi blanditiis, repellat vitae, saepe
+            harum quos, dicta error!
+          </p>
+        </div>
+        <footbar />
+      </div>
+    </div>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
 import appbar from "../components/Appbar.vue";
 import footbar from "../components/InitialFooter.vue";
-import News from "../components/GardenNews.vue";
 
 export default {
-  name: 'app',
-   data: () => {
-     return {
-       apiKey:'a60be4272ff046fb93e62cf4e8b13c22' //Get api key from newsapi.org
-     }
-   },
-  components: { appbar, footbar, News },
+  name: "app",
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  mounted() {
+    this.fetchNewsArticles();
+  },
+  components: { appbar, footbar },
+  methods: {
+    fetchNewsArticles() {
+      const apiKey = "a60be4272ff046fb93e62cf4e8b13c22";
+      const pageSize = 7;
+      const url = `https://newsapi.org/v2/everything?q=gardening&q=nature&pageSize=${pageSize}&apiKey=${apiKey}`;
+
+      axios
+        .get(url)
+        .then((response) => {
+          this.articles = response.data.articles;
+        })
+        .catch((error) => {
+          console.error("Error fetching news articles:", error);
+        });
+    },
+  },
 };
 </script>
 
-<style lang="scss">
-.left-sheet {
-  padding: 25px;
-  flex-direction: column;
+<style lang="scss" scoped>
+.appdiv {
   display: flex;
-  justify-content: center;
 }
 
-.left-div {
+.main {
+  position: relative;
+  z-index: 1;
+}
+
+.appbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+}
+
+.carousel-container {
+  margin-top: 64px;
+  padding: 20px;
+}
+
+.header {
+  margin-top: 64px;
   display: flex;
   flex-direction: column;
-  /* justify-content: flex-start; */
+  width: 100%;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
+  margin-bottom: 10px;
 }
 
-.right-sheet {
-  padding: 25px;
-  display: flex;
-  justify-content: center;
-}
-
-.right-div {
-  display: flex;
-  flex-direction: column;
-  /* justify-content: flex-start; */
-  justify-content: flex-start;
-}
-
-.news-sheet {
-  padding: 25px;
-  display: flex;
-  justify-content: center;
-}
-
-.news-div {
-  display: flex;
-  flex-direction: column;
-  /* justify-content: flex-start; */
-  justify-content: flex-start;
-}
-
-@import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
-
-body {
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    font-family: 'NTR', sans-serif;
-    background: #eee;
-}
-
-#app {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 1rem;
-  color: #333;
+.header img {
+  width: 100%;
   height: 100%;
-  background-color: #fff;
-  border: 2px solid #222;
-  border-radius: 20px;
-  box-shadow: 5px 10px 18px #222;
-  overflow-y: scroll;
-  overflow-x: hidden;
-} 
-  ::-webkit-scrollbar {
-    display: none;
-  }
+  object-fit: cover;
+}
+
+.v-responsive__content:is(div:not(:empty)) {
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.article-details {
+  position: absolute;
+  margin-top: 10px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px;
+  height: 100px;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.article-title {
+  color: white;
+  font-size: 18px;
+}
+.about {
+  position: relative;
+  z-index: 0;
+  padding: 20px;
+}
+.resources {
+  padding: 20px;
+}
 </style>
